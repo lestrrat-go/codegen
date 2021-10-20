0cdeb017a7a5694bf46a8d78d745650bd83ee26d
Daisuke Maki <lestrrat+github@gmail.com>
2021-10-20 21:13:39 +0900

    various improvements to work with real-life case

--- object.go
+++ object.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"log"
 	"sort"
 	"strings"
 
@@ -12,7 +11,9 @@ import (
 )
 
 var zerovals = map[string]string{
-	"string": `""`,
+	`string`:    `""`,
+	`time.Time`: `time.Time{}`,
+	`bool`:      `false`,
 }
 
 func RegisterZeroVal(typ, val string) {
@@ -30,6 +31,21 @@ type base struct {
 	name           string
 	exportedName   string
 	unexportedName string
+	comment        string
+	extras         map[string]interface{}
+}
+
+func (b *base) Initialize() {
+	b.name = ""
+	b.exportedName = ""
+	b.unexportedName = ""
+	b.comment = ""
+	b.extras = make(map[string]interface{})
+}
+
+func (b *base) Extra(s string) (interface{}, bool) {
+	v, ok := b.extras[s]
+	return v, ok
 }
 
 func (b *base) handleJSONField(dec *json.Decoder, field string) (bool, error) {
@@ -41,6 +57,8 @@ func (b *base) handleJSONField(dec *json.Decoder, field string) (bool, error) {
 		fref = &b.unexportedName
 	case "exported_name":
 		fref = &b.exportedName
+	case "comment":
+		fref = &b.comment
 	default:
 		return false, nil
 	}
@@ -48,6 +66,10 @@ func (b *base) handleJSONField(dec *json.Decoder, field string) (bool, error) {
 		return true, fmt.Errorf(`failed to decode field %q: %w`, field, err)
 	}
 	return true, nil
+}
+
+func (b *base) Comment() string {
+	return b.comment
 }
 
 func (b *base) Name(exported bool) string {
@@ -105,7 +127,12 @@ func (o *Object) Fields() []Field {
 	return o.fields
 }
 
+func (o *Object) AddField(f Field) {
+	o.fields = append(o.fields, f)
+}
+
 func (o *Object) UnmarshalJSON(data []byte) error {
+	o.base.Initialize()
 	dec := json.NewDecoder(bytes.NewReader(data))
 	tok, err := dec.Token()
 	if err != nil {
@@ -123,7 +150,6 @@ func (o *Object) UnmarshalJSON(data []byte) error {
 
 OUTER:
 	for {
-		log.Printf("OUTER")
 		tok, err := dec.Token()
 		if err != nil {
 			return fmt.Errorf(`failed to read next token: %w`, err)
@@ -152,48 +178,65 @@ OUTER:
 			case "array_of":
 				fref = &o.arrayOf
 			case "fields":
-				var list []json.RawMessage
-				if err := dec.Decode(&list); err != nil {
-					return fmt.Errorf(`failed to decode "fields" to list of messages`)
+				var fl FieldList
+				if err := dec.Decode(&fl); err != nil {
+					return fmt.Errorf(`failed to decode field list: %w`, err)
 				}
-
-				o.fields = make([]Field, 0, len(list))
-				// if the message contains `constant`, it's a constant
-				for i, raw := range list {
-					var probe struct {
-						Constant json.RawMessage `json:"constant"`
-					}
-					if err := json.Unmarshal(raw, &probe); err != nil {
-						return fmt.Errorf(`failed to decode probe for field %d: %w`, i+1, err)
-					}
-
-					var f Field
-					if len(probe.Constant) > 0 {
-						var c ConstantField
-						if err := json.Unmarshal(raw, &c); err != nil {
-							return fmt.Errorf(`failed to decode constant field %d: %w`, i+1, err)
-						}
-						f = &c
-					} else {
-						var s stdField
-						if err := json.Unmarshal(raw, &s); err != nil {
-							return fmt.Errorf(`failed to decode field %d: %w`, i+1, err)
-						}
-						f = &s
-					}
-
-					o.fields = append(o.fields, f)
-				}
-				log.Printf("parsed %d fields", len(o.fields))
+				o.fields = fl
 				continue OUTER
 			default:
-				return fmt.Errorf(`invalid object key %#v`, tok)
+				var v interface{}
+				if err := dec.Decode(&v); err != nil {
+					return fmt.Errorf(`failed to decode extra field %q: %w`, tok, err)
+				}
+				o.extras[tok] = v
+				continue OUTER
 			}
 
 			if err := dec.Decode(fref); err != nil {
 				return fmt.Errorf(`failed to decode field %q: %w`, tok, err)
 			}
 		}
+	}
+	return nil
+}
+
+type FieldList []Field
+
+func (l *FieldList) UnmarshalJSON(data []byte) error {
+	dec := json.NewDecoder(bytes.NewReader(data))
+
+	var list []json.RawMessage
+	if err := dec.Decode(&list); err != nil {
+		return fmt.Errorf(`failed to decode field list to list of messages`)
+	}
+
+	*l = make([]Field, 0, len(list))
+	// if the message contains `constant`, it's a constant
+	for i, raw := range list {
+		var probe struct {
+			Constant json.RawMessage `json:"constant"`
+		}
+		if err := json.Unmarshal(raw, &probe); err != nil {
+			return fmt.Errorf(`failed to decode probe for field %d: %w`, i+1, err)
+		}
+
+		var f Field
+		if len(probe.Constant) > 0 {
+			var c ConstantField
+			if err := json.Unmarshal(raw, &c); err != nil {
+				return fmt.Errorf(`failed to decode constant field %d: %w`, i+1, err)
+			}
+			f = &c
+		} else {
+			var s stdField
+			if err := json.Unmarshal(raw, &s); err != nil {
+				return fmt.Errorf(`failed to decode field %d: %w`, i+1, err)
+			}
+			f = &s
+		}
+
+		*l = append(*l, f)
 	}
 	return nil
 }
@@ -212,6 +255,10 @@ type Field interface {
 	JSON() string
 
 	GetterMethod(bool) string
+
+	Comment() string
+
+	Extra(string) (interface{}, bool)
 }
 
 type stdField struct {
@@ -285,9 +332,7 @@ func (f *stdField) handleJSONField(dec *json.Decoder, field string) (bool, error
 }
 
 func (f *stdField) UnmarshalJSON(data []byte) error {
-	f.name = ""
-	f.typ = ""
-	f.jsonName = ""
+	f.base.Initialize()
 
 	dec := json.NewDecoder(bytes.NewReader(data))
 	tok, err := dec.Token()
@@ -326,8 +371,14 @@ OUTER:
 			if handled {
 				continue OUTER
 			}
+			var v interface{}
+			if err := dec.Decode(&v); err != nil {
+				return fmt.Errorf(`failed to decode extra field %q: %w`, tok, err)
+			}
+			f.extras[tok] = v
+			continue OUTER
 		default:
-			return fmt.Errorf(`invalid field key %#v`, tok)
+			return fmt.Errorf(`invalid token: %#v`, tok)
 		}
 	}
 	return nil
@@ -387,7 +438,12 @@ OUTER:
 			case "constant":
 				fref = &f.value
 			default:
-				return fmt.Errorf(`invalid constant field key %#v`, tok)
+				var v interface{}
+				if err := dec.Decode(&v); err != nil {
+					return fmt.Errorf(`failed to decode extra field %q: %w`, tok, err)
+				}
+				f.extras[tok] = v
+				continue OUTER
 			}
 
 			if err := dec.Decode(fref); err != nil {
